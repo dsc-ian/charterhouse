0a6b7241e0cff239796b3983ff6eefcc81fa1842
Ian Villanueva <roland@dottystylecreative.com>
2015-09-08 13:55:20 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+$(document).ready(function(){
+
+
+	$('.bxslider').bxSlider({
+	    slideWidth: 320,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    moveSlides: 1,
+   		slideMargin: 10,
+	    pager: false,
+	    auto: true
+	  });
+
+
+
+
+
+
+});
